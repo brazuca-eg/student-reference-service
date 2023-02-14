dd80865f9dd0b6b26c621d80fea0debd665e58ca
Yehor_Kravchenko <Yehor_Kravchenko@epam.com>
2023-02-14 10:08:07 +0200

    DEV: Project initialization

---
+++ src/main/java/com/nure/kravchenko/student/reference/StudentReferenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nure.kravchenko.student.reference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentReferenceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentReferenceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nure/kravchenko/student/reference/StudentReferenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nure.kravchenko.student.reference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentReferenceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
