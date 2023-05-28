ff48f7ce57414a478e06ddd8003c630e3c70e1d9
Yehor Kravchenko <brazuca.egor@gmail.com>
2023-05-28 14:23:54 +0300

    DEV: provide changes for release

--- src/main/java/com/nure/kravchenko/student/reference/service/report/ReportService.java
+++ src/main/java/com/nure/kravchenko/student/reference/service/report/ReportService.java
@@ -1,6 +1,5 @@
 package com.nure.kravchenko.student.reference.service.report;
 
-import com.lowagie.text.DocumentException;
 import com.lowagie.text.pdf.BaseFont;
 import com.nure.kravchenko.student.reference.dto.ReportInformation;
 import com.nure.kravchenko.student.reference.entity.Request;
@@ -8,7 +7,6 @@ import com.nure.kravchenko.student.reference.entity.Student;
 import com.nure.kravchenko.student.reference.exception.InvalidProvidedDataException;
 import com.nure.kravchenko.student.reference.service.s3.StorageService;
 import com.nure.kravchenko.student.reference.service.utils.RandomUtils;
-import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
@@ -18,12 +16,10 @@ import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import javax.imageio.ImageIO;
-import javax.mail.MessagingException;
 import javax.transaction.Transactional;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.nio.file.Files;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.time.LocalDate;
 import java.util.Base64;
 import java.util.Locale;
@@ -32,7 +28,6 @@ import java.util.Objects;
 import static com.nure.kravchenko.student.reference.service.utils.ProjectConstants.UNDERSCORE;
 import static com.nure.kravchenko.student.reference.service.utils.ReportConstants.*;
 
-@Log4j
 @Service
 public class ReportService {
 
@@ -82,37 +77,16 @@ public class ReportService {
     }
 
     @Transactional
-    public String generatePdfFromHtml(Request request, byte[] signBytes) {
-        log.info("Starting generatePdf");
+    public String generatePdfFromHtml(Request request, byte[] signBytes) throws Exception {
         Student student = request.getStudent();
         if (student.isApproved()) {
             ReportInformation reportInformation = conversionService.convert(request, ReportInformation.class);
 
-            String directory = null;
-            try {
-                directory = new File(".").getCanonicalPath();
-                System.out.println(directory);
-            } catch (IOException e) {
-                throw new RuntimeException("error with base directory");
-            }
+            String directory = new File("./").getAbsolutePath();
 
-            String signImagePath = directory.substring(0, directory.length() - 1)
-                    + "src\\main\\resources\\templates\\";
-            ByteArrayInputStream bis = new ByteArrayInputStream(signBytes);
-            BufferedImage bImage2 = null;
-            try {
-                bImage2 = ImageIO.read(bis);
-                String signImage = signImagePath + "sign" + "." + JPG_EXTENSION;
-                ImageIO.write(bImage2, JPG_EXTENSION, new File(signImage));
-                File createdSignImage = new File(signImage);
-                String base64ImageCode = Base64.getEncoder()
-                        .encodeToString(Files.readAllBytes(createdSignImage.toPath()));
-                assert reportInformation != null;
-                reportInformation.setSign(base64ImageCode);
-                createdSignImage.delete();
-            } catch (IOException e) {
-                throw new RuntimeException("The image can't be read");
-            }
+            String base64ImageCode = Base64.getEncoder().encodeToString(signBytes);
+            assert reportInformation != null;
+            reportInformation.setSign(base64ImageCode);
 
             String path = directory.substring(0, directory.length() - 1) + "src\\main\\resources\\reports\\";
             LocalDate currentDate = LocalDate.now();
@@ -121,46 +95,23 @@ public class ReportService {
                     RandomUtils.getRandomNumber(MIN, MAX) + PDF_EXTENSION;
             String outputFolder = path + reportName;
 
-            OutputStream outputStream = null;
-            try {
-                outputStream = new FileOutputStream(outputFolder);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException("119");
-            }
+            OutputStream outputStream = new FileOutputStream(outputFolder);
 
             ClassLoader classLoader = getClass().getClassLoader();
             File fontFile = new File(Objects.requireNonNull(classLoader
                     .getResource(REPORT_FONT)).getFile());
 
             ITextRenderer renderer = new ITextRenderer();
-            try {
-                renderer.getFontResolver().addFont(fontFile.getPath(), BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-            } catch (DocumentException e) {
-                throw new RuntimeException("131 ex");
-            } catch (IOException e) {
-                throw new RuntimeException("133 ex");
-            }
+            renderer.getFontResolver().addFont(fontFile.getPath(), BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
             renderer.setDocumentFromString(parseThymeleafTemplate(reportInformation));
             renderer.layout();
-            try {
-                renderer.createPDF(outputStream);
-            } catch (DocumentException e) {
-                throw new RuntimeException("140 ex");
-            }
+            renderer.createPDF(outputStream);
 
-            try {
-                outputStream.close();
-            } catch (IOException e) {
-                throw new RuntimeException("146 ex");
-            }
+            outputStream.close();
 
-            try {
-                emailSenderService.sendMailWithAttachment("yehor.kravchenko@nure.ua",
-                        conversionService.convert(request, String.class),
-                        request.getReason().getDescription(), outputFolder);
-            } catch (MessagingException e) {
-                throw new RuntimeException("155");
-            }
+            emailSenderService.sendMailWithAttachment("yehor.kravchenko@nure.ua",
+                    conversionService.convert(request, String.class),
+                    request.getReason().getDescription(), outputFolder);
 
             File created = new File(outputFolder);
             storageService.uploadFile(created);
